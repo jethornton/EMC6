e818a0544207580575fab6267fe5bc7e9733e60c
John Thornton <dev@gnipsel.com>
2023-11-21 15:15:42 -0600

    initial work
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- debian/emc/usr/lib/python3/dist-packages/libemc/commands.py
+++ debian/emc/usr/lib/python3/dist-packages/libemc/commands.py
@@ -1,11 +1,11 @@
 
 class emc_control:
-	def __init__(self, emc):
+	def __init__(self, parent, emc):
 		self.emc = emc
 		self.command = emc.command()
 		self.status = emc.stat()
 		self.error = emc.error_channel()
-	def estop_toggle(parent):
+	def estop_toggle(self):
 		#state(int)
 		#Set the machine state. Machine state should be STATE_ESTOP, STATE_ESTOP_RESET, STATE_ON, or STATE_OFF.
 		self.command.state(self.emc.STATE_ESTOP_RESET)
--- debian/emc/usr/lib/python3/dist-packages/libemc/connections.py
+++ debian/emc/usr/lib/python3/dist-packages/libemc/connections.py
@@ -3,5 +3,5 @@ from functools import partial
 from libemc import commands
 
 def connect(parent):
-	parent.estop_pb.clicked.connect(partial(commands.emc_control.estop_toggle, parent))
+	parent.estop_pb.clicked.connect(commands.emc_control.estop_toggle)
 
