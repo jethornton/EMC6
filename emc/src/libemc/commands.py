17f2fe29b9f7201b4a0bdf1fa08424afbadbb3f6
John Thornton <dev@gnipsel.com>
2023-11-23 09:42:28 -0600

    create framework for buttons
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- emc/src/libemc/commands.py
+++ emc/src/libemc/commands.py
@@ -1,13 +1,34 @@
 
-class emc_control:
-	def __init__(self, parent, emc):
-		self.emc = emc
-		self.command = emc.command()
-		self.status = emc.stat()
-		self.error = emc.error_channel()
+import linuxcnc
+command = linuxcnc.command()
 
-	def estop_toggle(self):
-		#state(int)
-		#Set the machine state. Machine state should be STATE_ESTOP, STATE_ESTOP_RESET, STATE_ON, or STATE_OFF.
-		self.command.state(self.emc.STATE_ESTOP_RESET)
+def estop_toggle(parent):
+	if parent.status.task_state == linuxcnc.STATE_ESTOP:
+		command.state(linuxcnc.STATE_ESTOP_RESET)
+		#estop_pb.setStyleSheet('border-style: inset;')
+		parent.estop_pb.setStyleSheet('background-color: green;')
+		parent.energise_pb.setEnabled(True)
+		#print('red')
+	else:
+		command.state(linuxcnc.STATE_ESTOP)
+		#self.estop_pb.setStyleSheet('border-style: outset;')
+		#self.estop_pb.setStyleSheet('border: 1px inset black;')
+		#self.estop_pb.setStyleSheet('background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,stop: 0')
+		parent.estop_pb.setStyleSheet('background-color: red;')
+		parent.energise_pb.setEnabled(False)
+		parent.energise_pb.setStyleSheet('background-color: ;')
+		#print('green')
+
+def power_toggle(parent):
+	print(parent.status.task_state)
+	#print(linuxcnc.STATE_OFF)
+	#print(linuxcnc.STATE_ON)
+	if parent.status.task_state == linuxcnc. STATE_ESTOP_RESET:
+		command.state(linuxcnc.STATE_ON)
+		parent.energise_pb.setStyleSheet('background-color: green;')
+		#print('h')
+	else:
+		command.state(linuxcnc.STATE_OFF)
+		parent.energise_pb.setStyleSheet('background-color: ;')
+		#print('t')
 
--- emc/src/libemc/connections.py
+++ emc/src/libemc/connections.py
@@ -1,7 +1,22 @@
 from functools import partial
 
+from PyQt6.QtWidgets import QPushButton
+
 from libemc import commands
 
 def connect(parent):
-	parent.estop_pb.clicked.connect(commands.emc_control.estop_toggle)
+
+	connections = {'estop_pb': 'estop_toggle',
+	'energise_pb': 'power_toggle'}
+	pushbuttons = []
+	children = parent.findChildren(QPushButton)
+	for child in children:
+		pushbuttons.append(child.objectName())
+
+	for pb in pushbuttons:
+		if pb in connections:
+			#print(pb)
+			#print(connections[pb])
+			getattr(parent, pb).clicked.connect(partial(getattr(commands, connections[pb]), parent))
+	#parent.estop_pb.clicked.connect(partial(commands.estop_toggle, parent))
 
