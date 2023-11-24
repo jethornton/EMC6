56777c5b63496d15ce407b65e27de942240a2568
John Thornton <dev@gnipsel.com>
2023-11-24 08:00:50 -0600

    load ui file from config directory
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- emc/src/libemc/commands.py
+++ emc/src/libemc/commands.py
@@ -5,30 +5,23 @@ command = linuxcnc.command()
 def estop_toggle(parent):
 	if parent.status.task_state == linuxcnc.STATE_ESTOP:
 		command.state(linuxcnc.STATE_ESTOP_RESET)
-		#estop_pb.setStyleSheet('border-style: inset;')
 		parent.estop_pb.setStyleSheet('background-color: green;')
-		parent.energise_pb.setEnabled(True)
-		#print('red')
+		parent.power_pb.setEnabled(True)
 	else:
 		command.state(linuxcnc.STATE_ESTOP)
-		#self.estop_pb.setStyleSheet('border-style: outset;')
-		#self.estop_pb.setStyleSheet('border: 1px inset black;')
-		#self.estop_pb.setStyleSheet('background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,stop: 0')
 		parent.estop_pb.setStyleSheet('background-color: red;')
-		parent.energise_pb.setEnabled(False)
-		parent.energise_pb.setStyleSheet('background-color: ;')
-		#print('green')
+		parent.power_pb.setEnabled(False)
+		parent.power_pb.setStyleSheet('background-color: ;')
+		parent.power_pb.setText('Power Off')
 
 def power_toggle(parent):
-	print(parent.status.task_state)
-	#print(linuxcnc.STATE_OFF)
-	#print(linuxcnc.STATE_ON)
 	if parent.status.task_state == linuxcnc. STATE_ESTOP_RESET:
 		command.state(linuxcnc.STATE_ON)
-		parent.energise_pb.setStyleSheet('background-color: green;')
-		#print('h')
+		parent.power_pb.setStyleSheet('background-color: green;')
+		parent.power_pb.setText('Power On')
 	else:
 		command.state(linuxcnc.STATE_OFF)
-		parent.energise_pb.setStyleSheet('background-color: ;')
-		#print('t')
+		parent.power_pb.setStyleSheet('background-color: ;')
+		parent.power_pb.setText('Power Off')
+
 
--- emc/src/libemc/connections.py
+++ emc/src/libemc/connections.py
@@ -1,22 +1,26 @@
 from functools import partial
 
 from PyQt6.QtWidgets import QPushButton
+from PyQt6.QtGui import QAction
 
 from libemc import commands
 
 def connect(parent):
-
-	connections = {'estop_pb': 'estop_toggle',
-	'energise_pb': 'power_toggle'}
+	controls = {'estop_pb': 'estop_toggle',
+	'power_pb': 'power_toggle'}
 	pushbuttons = []
 	children = parent.findChildren(QPushButton)
 	for child in children:
 		pushbuttons.append(child.objectName())
 
 	for pb in pushbuttons:
-		if pb in connections:
-			#print(pb)
-			#print(connections[pb])
-			getattr(parent, pb).clicked.connect(partial(getattr(commands, connections[pb]), parent))
-	#parent.estop_pb.clicked.connect(partial(commands.estop_toggle, parent))
+		if pb in controls:
+			getattr(parent, pb).clicked.connect(partial(getattr(commands, controls[pb]), parent))
+
+	# Menu Items
+	actions = []
+	for action in parent.findChildren(QAction):
+		if action.objectName():
+			actions.append(action.objectName())
+	#print(actions)
 
